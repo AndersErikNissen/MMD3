cbfe4dfc9517b256491c40d34ea90e7e03738036
Anders <1086088@ucn.dk>
2022-01-06 03:58:13 +0100

    updated groundstructure + root colors

---
+++ src/assets/api/apiroutes.js
@@ -0,0 +1,29 @@
+/*
+    Inspiration from: https://medium.com/js-dojo/javascript-best-practises-strongly-typed-routes-a6151883794
+
+    Reused from Github: AndersErikNissen/portfolio-v3
+*/
+const
+    // Blocks
+    // "Bulding Blocks" values start with _.
+    _status = "?status=publish",
+    _category = "&categories=",
+    _categoryV3 = _category + "81",
+    _categoryCases = _category + "82",
+    _categoryDesigns = _category + "83",
+    _perPage = "&per_page=50",
+    _slug = slug => "&slug=" + slug,
+
+    // Base
+    apiBase = "https://skole.aenders.dk/wp-json/wp/v2/posts",
+    apiQueryBase = _status + _category + _perPage,
+
+    // Collection
+    ApiGet = {
+        byId: id => apiBase + '/' + id + apiQueryBase,
+        bySlug: slug => apiBase + apiQueryBase + _slug(slug),
+        allV3: apiBase + apiQueryBase + _categoryV3,
+        allCases: apiBase + apiQueryBase + _categoryCases,
+        allDesigns: apiBase + apiQueryBase + _categoryDesigns,
+    }
+export default ApiGet;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store/store'
 
-createApp(App).mount('#app')
+import '@/assets/styles/main.scss'
+import '@/assets/styles/root.css'
+
+createApp(App).use(store).use(router).mount('#app')
