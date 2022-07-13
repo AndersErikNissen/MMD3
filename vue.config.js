e07d7a1e2be632fd656dad460db2fab301032637
Anders Erik Nissen <73883535+AndersErikNissen@users.noreply.github.com>
2022-07-13 15:16:34 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    publicPath: '/class/mmda0920/1086088/Sem3/eksamen/Rubrics/mmd3_version2'
+    // publicPath: '/class/mmda0920/1086088/Sem3/eksamen/Rubrics/mmd3_version2'
+    publicPath: '/'
 }
 
 // as for now, if you had access to the student server:
